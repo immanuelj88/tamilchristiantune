33c303e6409f637d6cca19f73275382cf0a69071
immanuelj88 <immanuelj88coc@gmail.com>
2018-08-06 18:24:18 +0530

    Initial commit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Socialite\Facades\Socialite;
+
+
+class WelcomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function welcome()
+    {
+        return view('welcome');
+    }
+}
